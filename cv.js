50602deb1c19a86ff811cd09513b14a354d696b2
ravitamos <ravitam@post.bgu.ac.il>
2021-12-03 17:58:46 +0200

    exercise 6

---
+++ cv.js
@@ -0,0 +1,8 @@
+
+function validEmail(EmailC, note1){
+    const inpObj= document.getElementById("EmailC");
+    if(!inpObg.checkValidity()){
+        document.getElementById(note1).innerHTML= inpObj.validationMwssage;
+    }
+}
+
